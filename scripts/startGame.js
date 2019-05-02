517ff17593c249a7af36dc278b06d05f1a468875
Wesley Ferreira <wsf.ley@gmail.com>
2019-05-02 11:30:14 -0300

    selecionamento de imagens e defnindo tamanho dific

--- scripts/startGame.js
+++ scripts/startGame.js
@@ -1,10 +1,10 @@
 function startGame() {
     data = getConfigGame()
     setScreenGame(data)
-        // showScreenGame()
-        // gameRunning()
-        // isWinner()
-        // restart()
+    // showScreenGame()
+    // gameRunning()
+    // isWinner()
+    // restart()
 }
 
 function getConfigGame() {
@@ -12,9 +12,10 @@ function getConfigGame() {
 }
 
 function setScreenGame(d) {
-    let hard = 8;
+    let hard = setDificult(d.dificult)
+    // let hard = 6;
     let sGame = document.getElementById('screen-game')
-    for (let i = 0; i < hard; i++) {
+    for (let indexI = 0; indexI < hard; indexI++) {
         let trElement = document.createElement('tr')
         trElement.className = 'img-coluns'
         sGame.appendChild(trElement)
@@ -23,14 +24,34 @@ function setScreenGame(d) {
             tdElement.className = 'td-cells'
             let img = document.createElement('img')
             img.className = 'img-cells'
-            img.alt = 'a' + indexJ
-            img.src = 'assets/gifs/gif-ground/00.gif'
+            img.alt = indexI + ' ' + indexJ
+            // img.src = 'assets/gifs/gif-ground/00.gif'
+            img.src = getArrayTable(d)[indexJ]
             tdElement.appendChild(img)
             trElement.appendChild(tdElement)
         }
 
     }
-
-
-
+    getArrayTable(d)
 }
+
+function setDificult(d) {
+    return d + 5
+}
+function getImage(indexImg) {
+    let pathImg = 'assets/gifs/gif-ground/' + '0' + indexImg + '.gif'
+    return pathImg
+}
+function getArrayTable(d) {
+    let qntdImg = (setDificult(d.dificult) * setDificult(d.dificult)) / 2
+    let imgs = new Array()
+    for (let indexI = 0; indexI < qntdImg; indexI++) {
+        let value = getImage(indexI)
+        imgs.push(value)
+    }
+    console.log(imgs)
+    return imgs
+}
+
+
+
